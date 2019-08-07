85d695273df101479ac191db2214b5812b43351a
suppergerrie2 <gerjan2001@gmail.com>
2019-08-07 21:12:05 +0200

    Added GetOrganismsRequest/Response

--- src/main/java/net/neuralm/client/messages/requests/GetOrganismsRequest.java
+++ src/main/java/net/neuralm/client/messages/requests/GetOrganismsRequest.java
@@ -1,8 +1,15 @@
 package net.neuralm.client.messages.requests;
 
+import java.util.UUID;
+
 public class GetOrganismsRequest extends Request {
 
-    public GetOrganismsRequest() {
+    public UUID trainingSessionID;
 
+    public int amount;
+
+    public GetOrganismsRequest(UUID trainingSessionID, int amount) {
+        this.trainingSessionID = trainingSessionID;
+        this.amount = amount;
     }
 }
--- src/main/java/net/neuralm/client/messages/responses/GetOrganismsResponse.java
+++ src/main/java/net/neuralm/client/messages/responses/GetOrganismsResponse.java
@@ -1,5 +1,14 @@
 package net.neuralm.client.messages.responses;
 
+import java.util.List;
+import net.neuralm.client.neat.Organism;
+
 public class GetOrganismsResponse extends Response {
+
+    private List<Organism> organisms;
+
+    public List<Organism> getOrganisms() {
+        return organisms;
+    }
 
 }
